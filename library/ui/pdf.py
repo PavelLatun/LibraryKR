005a94812f79a1952ac3c98e832c9e4bbb2c4400
PavelLatun <LatunPaxa@bk.ru>
2022-12-06 20:07:46 +0300

    KontrRab

--- library/ui/pdf.py
+++ library/ui/pdf.py
@@ -10,14 +10,14 @@ class PdfPrinter:
     def print_book(file_name: str, book):
         book_text = BookFormatter.format_book(book)
         pdf = PdfPrinter.__create_pdf()
-        pdf.cell(250, 30, "Your book:", ln=1, align="L")
+        pdf.cell(250, 30, "Ваша книга:", ln=1, align="L")
         pdf.cell(250, 15, book_text, ln=1, align="L")
         pdf.output(file_name)
 
     @staticmethod
     def print_books(file_name: str, books):
         pdf = PdfPrinter.__create_pdf()
-        pdf.cell(250, 30, "Your library:", ln=1, align="L")
+        pdf.cell(250, 30, "Ваша библиотека:", ln=1, align="L")
         for book in books:
             pdf.cell(250, 15, BookFormatter.format_book(book), ln=1, align="L")
         pdf.output(file_name)
@@ -26,6 +26,7 @@ class PdfPrinter:
     def __create_pdf():
         pdf = FPDF()
         pdf.add_page()
-        pdf.set_font("Arial", size=16)
+        pdf.add_font('DejaVu', style="", fname="ui/font/DejaVuSansCondensed.ttf", uni=True)
+        pdf.set_font('DejaVu', '', 16)
 
         return pdf
